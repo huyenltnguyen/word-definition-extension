10af5095b4bb883d197972233589dcfe1f69900e
huyenltnguyen <25715018+huyenltnguyen@users.noreply.github.com>
2018-02-08 10:08:30 +0700

    get maximum 3 definitions

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 console.log('Word Definition Extension is running');
 
 const getSelectedText = () => {
-  const selectedText = window.getSelection().toString().trim();
+  const selectedText = window.getSelection().toString().trim().toLowerCase();
 
   if (selectedText.length > 0) {
     console.log(selectedText);
--- popup.js
+++ popup.js
@@ -2,19 +2,19 @@ const bgPage = chrome.extension.getBackgroundPage();
 const word = bgPage.word;
 
 const getDefinition = () => {
-  const apiUrl = `http://api.wordnik.com:80/v4/word.json/${word}/definitions?limit=200&includeRelated=false&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
+  const apiUrl = `http://api.wordnik.com:80/v4/word.json/${word}/definitions?limit=3&includeRelated=false&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`;
 
   fetch(apiUrl)
   .then((res) => res.json())
-  .then((data) => createPara(data[0].text))
+  .then((data) => data.forEach((d) => createListItem(d.text)))
   .catch((err) => console.log(err));
 };
 
-const createPara = (def) => {
-  const para = document.createElement('p');
-  const paraText = document.createTextNode(def);
-  para.appendChild(paraText);
-  document.body.appendChild(para);
+const createListItem = (def) => {
+  const item = document.createElement('li');
+  const itemText = document.createTextNode(def);
+  item.appendChild(itemText);
+  document.querySelector('ul').appendChild(item);
 };
 
 
